b202f1cee89eafb88ec7790d6823efd053a0a1f9
Cristopher Yates <cmoyates@gmail.com>
2021-09-02 10:54:35 -0230

    Added realtime updating when new suggestions are added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,17 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
+import io from 'socket.io-client';
+
+let socket;
 
 const Home = (props) => {
 
     const [suggestions, setSuggestions] = useState([]);
     const [username, setUsername] = useState("");
     const [content, setContent] = useState("");
+
+    const ENDPOINT = "https://still-sands-81765.herokuapp.com";
 
     const submitSuggestion = async (suggestion) => {
         console.log("So far so good");
@@ -47,43 +52,33 @@ const Home = (props) => {
 
     useEffect(() => {
         fetchSuggestions();
+
+        socket = io(ENDPOINT);
+
+        console.log(socket);
+
+        socket.emit('join', () => {
+            console.log("Socket connection established");
+        });
+
+        return () => {
+            socket.disconnect();
+            socket.off();
+        }
+
     },[]);
+
+    useEffect(() => {
+        socket.off('suggestionAdded').on('suggestionAdded', (suggestion) => {
+            setSuggestions([suggestion, ...suggestions]);
+        })
+    })
 
     return (
         <Grid container direction="column" justifyContent="center" alignItems="center">
-            {/*<Grid item>
-                <br/>
-                <TextField
-                label="Username"
-                variant="outlined"
-                autoComplete='off'
-                value={username}
-                onChange={(event) => {
-                    setUsername(event.target.value);
-                }}
-                />
-                <br/>
-                <br/>
-                <TextField
-                label="Suggestion"
-                variant="outlined"
-                autoComplete='off'
-                autoFocus
-                multiline
-                value={content}
-                onChange={(event) => {
-                    setContent(event.target.value);
-                }}
-                />
-                <br/>
-                <br/>
-                <Button onClick={async ()=>{
-                    await submitSuggestion({user_name: username, content});
-                }}>Submit</Button>
-            </Grid>*/}
             <Grid item>
             <h1>Idea List</h1>
-                <Paper style={{minWidth: "35vw", marginBottom: 20}}>
+                <Paper style={{width: 600, maxWidth: 600, marginBottom: 20}}>
                     <List>
                         {(suggestions.length === 0) ? 
                             (<ListItem>
@@ -93,8 +88,8 @@ const Home = (props) => {
 
                                 const date = new Date(item.ts);
 
-                                return (<div>
-                                    <ListItem key={index}>
+                                return (<div key={index}>
+                                    <ListItem>
                                         <ListItemText>
                                             <h2 style={{marginBottom: -5}}>{item.content}</h2>
                                             {item.user_name} @ {date.toLocaleString()}
@@ -112,3 +107,37 @@ const Home = (props) => {
 }
 
 export default Home
+
+
+
+
+/*<Grid item>
+    <br/>
+    <TextField
+    label="Username"
+    variant="outlined"
+    autoComplete='off'
+    value={username}
+    onChange={(event) => {
+        setUsername(event.target.value);
+    }}
+    />
+    <br/>
+    <br/>
+    <TextField
+    label="Suggestion"
+    variant="outlined"
+    autoComplete='off'
+    autoFocus
+    multiline
+    value={content}
+    onChange={(event) => {
+        setContent(event.target.value);
+    }}
+    />
+    <br/>
+    <br/>
+    <Button onClick={async ()=>{
+        await submitSuggestion({user_name: username, content});
+    }}>Submit</Button>
+</Grid>*/
